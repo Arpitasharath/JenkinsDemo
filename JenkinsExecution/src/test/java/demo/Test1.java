c51ce811cdd329d635d22ceba43d2a2a5d696eaa
Arpitasharath <arpitasharath@gmail.com>
2024-09-18 19:23:07 +0530

    push

---
+++ JenkinsExecution/src/test/java/demo/Test1.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+		@Test
+		public void createProduct()
+		{
+			System.out.println("Product as been created Successfully");
+		}
+		
+	}
+
+
